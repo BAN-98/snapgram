6744527a907a8ad0ece1b3411689a4210f49afa9
ryshen <ryshen98@outlook.com>
2024-04-15 10:39:35 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import "@fontsource/poppins";
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<} />
+
+        {/* private routes */}
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
